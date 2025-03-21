3286242e963eca4806c5b2eb1af443289371653d
baxthus <baxt@baxt.fun>
2025-03-20 21:36:45 -0300

    chore: update dependencies and rename findAllFiles to findRecursively for clarity

--- src/utils/findRecursively.ts
+++ src/utils/findRecursively.ts
@@ -1,7 +1,7 @@
 import { readdir } from 'fs/promises';
 import path from 'path';
 
-export default async function findAllFiles(dir: string): Promise<string[]> {
+export default async function findRecursively(dir: string): Promise<string[]> {
   const files = await readdir(dir, { withFileTypes: true });
   const tsFiles: string[] = [];
 
@@ -10,7 +10,7 @@ export default async function findAllFiles(dir: string): Promise<string[]> {
       const filePath = path.join(dir, file.name);
 
       if (file.isDirectory()) {
-        const nestedFiles = await findAllFiles(filePath);
+        const nestedFiles = await findRecursively(filePath);
         tsFiles.push(...nestedFiles);
       } else if (file.name.endsWith('.ts')) {
         tsFiles.push(filePath);
