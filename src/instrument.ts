8ac98047d78c2c2642a2ad12ec7f3be32a65d68e
baxthus <baxt@baxt.fun>
2025-04-16 20:15:22 -0300

    fix: comment out debug option in Sentry initialization

--- src/instrument.ts
+++ src/instrument.ts
@@ -3,6 +3,6 @@ import * as Sentry from '@sentry/bun';
 Sentry.init({
   dsn: Bun.env.SENTRY_DSN,
   enabled: Bun.env.NODE_ENV === 'production',
-  debug: Bun.env.NODE_ENV !== 'production',
+  // debug: Bun.env.NODE_ENV !== 'production',
   tracesSampleRate: 1.0, // Adjust in the future
 });
