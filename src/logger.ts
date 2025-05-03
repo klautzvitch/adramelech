8b14a7f9826bee688ff8e32301f839f016e4a963
baxthus <baxt@baxt.fun>
2025-05-02 21:23:34 -0300

    fix: better Sentry integration with new logging system
    Closes KLAUTZ-104

--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,45 @@
+import { addBreadcrumb, captureException, captureMessage } from '@sentry/bun';
 import chalk from 'chalk';
 
 type LogParams = [message?: unknown, ...params: unknown[]];
 
 function log(...params: LogParams) {
-  return console.log(...params);
+  console.log(...params);
 }
 
 function info(...params: LogParams) {
-  return log(chalk.blue(''), ...params);
+  log(chalk.blue(''), ...params);
 }
 function success(...params: LogParams) {
-  return log(chalk.green(''), ...params);
+  log(chalk.green(''), ...params);
 }
 function warn(...params: LogParams) {
-  return log(chalk.yellow(''), ...params);
+  log(chalk.yellow(''), ...params);
+  captureMessage(`Warning: ${String(params[0])}`, 'warning');
+  addBreadcrumb({
+    category: 'log',
+    level: 'warning',
+    message: params.map(String).join(' '),
+  });
 }
 function error(...params: LogParams) {
-  return log(chalk.red(''), ...params);
+  log(chalk.red(''), ...params);
+
+  const potentialError = params[0];
+  const extraData =
+    params.length > 1
+      ? {
+          additional_info: params.slice(1),
+        }
+      : undefined;
+
+  if (potentialError instanceof Error) {
+    captureException(potentialError, { extra: extraData });
+  } else {
+    const message = `Logged Error: ${params.map((p) => (typeof p === 'string' ? p : JSON.stringify(p))).join(' ')}`;
+    captureMessage(message, { level: 'error', extra: extraData });
+    captureException(new Error(message), { extra: extraData });
+  }
 }
 
 export default {
